5befe2b3ef00da67807851378f6706fab6cbefe7
mahikute505-art <mahikute505@gmaail.com>
2025-11-11 19:07:29 +0530

    GoogleForm

---
+++ tests/googleForm.spec.js
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+
+test('insert from', async ({ page }) => {
+
+   await page.goto('https://practice.expandtesting.com/inputs');
+await page.locator('#input-number').fill('8080496083');
+await page.locator('#input-text').fill('mohit');
+await page.locator('#input-password').fill('mk010230');
+
+
+//await page.locator('#input-date').fill('11-11-2000');
+await page.locator('#btn-display-inputs').click();
+
+await page.waitForTimeout(5000);
+
+await page.locator('#btn-clear-inputs').click();
+
+});
+ test ('login page', async({page}) =>
+{ 
+await page.goto('https://practice.expandtesting.com/login');
+
+await page.locator('#username').fill('practice');
+await page.locator('#password').fill('SuperSecretPassword!');
+
+await page.waitForTimeout(5000);
+await page.locator('#submit-login').click();
+
+await page.waitForTimeout(5000);
+
+await page.locator('.icon-2x.icon-signout').click();
+
+});
+
+test ('register form', async ({page}) =>
+{
+await  page.goto('https://demo.automationtesting.in/Register.html');
+
+await page.getByPlaceholder('First Name').fill('mohit');
+await page.getByPlaceholder('Last Name').fill('kute');
+
+
+//const locator = page.locator('textarea[ng-model="Dress"]');
+
+ //await page.locator('#type=email').fill('mohitkute01@gmail.com');
+
+});
