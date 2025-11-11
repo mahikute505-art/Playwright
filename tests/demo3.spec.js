9f13b66eccb0d7e5b64445b812e290ed17520163
mahikute505-art <mahikute505@gmaail.com>
2025-11-11 14:58:49 +0530

    demo3

--- tests/demo3.spec.js
+++ tests/demo3.spec.js
@@ -6,16 +6,16 @@ test ('Built-in-Locators',async({page}) =>
 
         await page.goto('https://www.demoblaze.com/index.html');
 
-       //await page.locator('#signin2').click();
+       await page.locator('#signin2').click();
 
-     //   await page.getByText(' PRODUCT STORE').click();
-    // await page.getByLabel('Username:').fill('mohit');
-//await page.getByRole('link' ,{name:'About us'}).click();
+        await page.getByText(' PRODUCT STORE').click();
+     await page.getByLabel('Username:').fill('mohit');
+await page.getByRole('link' ,{name:'About us'}).click();
 
-//const playButton = page.getByTitle('Play Video');
+const playButton = page.getByTitle('Play Video');
 
-//await playButton.click();
-//await page.locator('.nav-link').click();
+await playButton.click();
+await page.locator('.nav-link').click();
 
 
 
