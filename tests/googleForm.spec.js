e996a77d4460fd4a82f3f7f72a50fb2e4a715e97
mahikute505-art <mahikute505@gmaail.com>
2025-11-12 11:53:01 +0530

    register

--- tests/googleForm.spec.js
+++ tests/googleForm.spec.js
@@ -34,17 +34,62 @@ await page.locator('.icon-2x.icon-signout').click();
 
 test ('register form', async ({page}) =>
 {
-await  page.goto('https://demo.automationtesting.in/Register.html');
+await  page.goto('https://demo.automationtesting.in/Register.html');// webside link
 
-await page.getByPlaceholder('First Name').fill('mohit');
-await page.getByPlaceholder('Last Name').fill('kute');
+await page.getByPlaceholder('First Name').fill('mohit');//First name
+await page.getByPlaceholder('Last Name').fill('kute');//last name
+
+await page.locator('textarea[ng-model="Adress"]').fill('pune maharshtra 412208'); //Address
+
+ await page.locator('input[type="email"]').fill('mohitkute01@gmail.com'); //email ID
+
+ await page.locator('input[type="tel"]').fill('8080496083'); //phone number
+
+ await page.locator('input[value="Male"]').check(); // for male
+
+ //await page.locator('input[value="FeMale"]').check(); // for female 
+
+ //await page.locator('#msdd').fill('english , marathi , Hindi');
+
+ await page.locator('#checkbox1').check(); // check the hobbies
+ await page.locator('#checkbox2').check();
+ await page.locator('#checkbox3').check();
+
+const items = page.locator('li.ng-scope'); // all <li> inside the list
+const count = await items.count();
+console.log('Number of language:', count);
+await page.locator('#msdd').click();
+ await page.locator('li', { hasText: 'Hindi'}).click(); //select the language
+await page.locator('li', { hasText: 'Danish'}).click();
+
+//await page.waitForTimeout(5000);
+
+await page.locator('#Skills').selectOption('HTML'); //select skill
+await page.waitForTimeout(5000);
+//await page.locator('#countries').click(); // click on select countries 
+
+//await page.locator('.select2-selection').click(); //select countries
+//await page.locator('.select2-search__field').fill('India'); 
+//await page.locator('.select2-results__option', { hasText: 'India' }).click();// select countries 
 
 
-//const locator = page.locator('textarea[ng-model="Dress"]');
+await page.locator('#yearbox').selectOption('2000');//select year
+await page.getByPlaceholder('Month').selectOption('November');//select mounths
+await page.locator('#daybox').selectOption('23');//select days
 
- //await page.locator('#type=email').fill('mohitkute01@gmail.com');
+await page.locator('#firstpassword').fill('mahik505');//enter password
+await page.locator('#secondpassword').fill('mahik505');//enter repassword
 
-});
+await page.locator('#submitbtn').click();//click on submit button
+
+
+await page.waitForTimeout(5000); // wait for 5 sec.
+await page.locator('#Button1').click(); //click on clear button
+
+
+await page.locator('#imagesrc').setInputFiles("./tests/files/1.png"); //img uploding..
+
+}); //test close
 
 test ('file submit', async ({page}) =>  
 {
