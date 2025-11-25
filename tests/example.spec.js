0980cd9a322407edab66d0bc7dd49700b4be4a92
mahikute505-art <mahikute505@gmaail.com>
2025-11-25 13:53:08 +0530

    22

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,6 +6,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
